513bf563ed38af1e11e14e81cd3b315811bf4150
Lucas Nascimento <lucabelezal@gmail.com>
2021-03-20 23:41:58 -0300

    add test example

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -3,12 +3,7 @@
  */
 
 import 'react-native';
-import React from 'react';
-import App from '../src/App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+it('test', () => {
+  expect(1+1).toBe(2);
 });
