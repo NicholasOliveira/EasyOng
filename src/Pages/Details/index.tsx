592d955bfc6f1c254f3925e984cad7f281d76bdb
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-04-14 19:02:21 -0300

    chore: updae files images mocked

--- src/Pages/Details/index.tsx
+++ src/Pages/Details/index.tsx
@@ -25,11 +25,23 @@ import Wrapper from '../../components/Wrapper';
 const BackIcon = (props: any) => <Icon {...props} name="arrow-back" />;
 
 function DetailsScreen({route, navigation}: any) {
-  const {itemId} = route.params;
+  const {itemId, index} = route.params;
   const {Favorites, setFavorites}: any = useContext(FavoritesContext);
   const {User}: any = useContext(UsersContext);
   const [Ongloading, setOngLoad] = useState<any>(true);
   const [Ong, setOng] = useState<any>({});
+
+  const pictures = [
+    'https://cdn-istoe-ssl.akamaized.net/wp-content/uploads/sites/14/2020/04/ajuda-ongs.jpg',
+    'https://impactosocial.esolidar.com/wp-content/uploads/2020/05/ONG-confiavel-como-transmitir-a-sua-mensagem.png',
+    'https://impactosocial.esolidar.com/wp-content/uploads/2020/06/O-papel-das-ONGs-nas-metas-globais-das-Nac%CC%A7o%CC%83es-Unidas.jpg',
+    'https://nossacausa.com/wp-content/uploads/2014/09/ongs.jpg',
+    'https://observatorio3setor.org.br/wp-content/uploads/2017/03/2013_September_SocialGood_SocialMediaForSocialGood_Image-portal.jpg',
+    'https://static.poder360.com.br/2019/01/ONG-Brasil.png',
+    'https://exame.com/wp-content/uploads/2020/12/ongs-empresas-filantropia.jpg',
+    'https://www.tamarthi.com.br/wp-content/uploads/2019/12/como-abrir-uma-ong.jpg',
+    'https://static.wixstatic.com/media/bec156_a6e0560b562b402c850245c15d8bc600~mv2.png/v1/fill/w_689,h_435,al_c,lg_1,q_90/bec156_a6e0560b562b402c850245c15d8bc600~mv2.webp',
+  ];
 
   useEffect(() => {
     //setFavorites(undefined);
@@ -102,7 +114,7 @@ function DetailsScreen({route, navigation}: any) {
           <ScrollView style={styles.scrollView}>
             <Layout style={styles.layout}>
               <View style={styles.container}>
-                <ImgView source={{uri: Ong?.pictures[0]?.url}} />
+                <ImgView source={{uri: pictures[index]}} />
                 <LinearGradient
                   colors={['transparent', 'black']}
                   style={styles.linearGradient}>
--- src/Pages/Explore/index.tsx
+++ src/Pages/Explore/index.tsx
@@ -25,9 +25,21 @@ import SearchBar from '../../components/SearchBar';
 import {OngsContext} from '../../Contexts/index';
 
 export const ExploreScreen = ({navigation}: any) => {
+  const pictures = [
+    'https://cdn-istoe-ssl.akamaized.net/wp-content/uploads/sites/14/2020/04/ajuda-ongs.jpg',
+    'https://impactosocial.esolidar.com/wp-content/uploads/2020/05/ONG-confiavel-como-transmitir-a-sua-mensagem.png',
+    'https://impactosocial.esolidar.com/wp-content/uploads/2020/06/O-papel-das-ONGs-nas-metas-globais-das-Nac%CC%A7o%CC%83es-Unidas.jpg',
+    'https://nossacausa.com/wp-content/uploads/2014/09/ongs.jpg',
+    'https://observatorio3setor.org.br/wp-content/uploads/2017/03/2013_September_SocialGood_SocialMediaForSocialGood_Image-portal.jpg',
+    'https://static.poder360.com.br/2019/01/ONG-Brasil.png',
+    'https://exame.com/wp-content/uploads/2020/12/ongs-empresas-filantropia.jpg',
+    'https://www.tamarthi.com.br/wp-content/uploads/2019/12/como-abrir-uma-ong.jpg',
+    'https://static.wixstatic.com/media/bec156_a6e0560b562b402c850245c15d8bc600~mv2.png/v1/fill/w_689,h_435,al_c,lg_1,q_90/bec156_a6e0560b562b402c850245c15d8bc600~mv2.webp',
+  ];
+
   const {Ongs}: any = useContext(OngsContext);
-  const navigateDetails = (id: number) => {
-    navigation.navigate('Details', {itemId: id});
+  const navigateDetails = (id: number, index: any) => {
+    navigation.navigate('Details', {itemId: id, index});
   };
   const FilterIcon = (props: any) => (
     <Icon fill="#ffffff" name="options-2" {...props} />
@@ -37,9 +49,15 @@ export const ExploreScreen = ({navigation}: any) => {
     <Icon fill="#ffffff" name="chevron-down" {...props} />
   );
 
+  function getRandomInt() {
+    let min = Math.ceil(1);
+    let max = Math.floor(3);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   const renderHorizontalOngItem = (info: any) => (
-    <OngCardItem onPress={() => navigateDetails(info.item.id)}>
-      <ImageUI source={{uri: info?.item?.pictures[0]?.url}} />
+    <OngCardItem onPress={() => navigateDetails(info.item.id, getRandomInt())}>
+      <ImageUI source={{uri: pictures[getRandomInt()]}} />
       <ItemTitle>{info.item.name}</ItemTitle>
       <ItemDescription>{info.item.description.substr(0, 55)}</ItemDescription>
     </OngCardItem>
@@ -85,17 +103,23 @@ export const ExploreScreen = ({navigation}: any) => {
                 <TextView>Veja mais</TextView>
               </ViewFlex>
               <ListCardItem>
-                {[...Ongs].sort().map((Ong: any) => (
-                  <CardItem
-                    onPress={() => navigateDetails(Ong.id)}
-                    key={Ong.id}>
-                    <ImageUI source={{uri: Ong?.pictures[0]?.url}} />
-                    <ItemTitle>{Ong.name}</ItemTitle>
-                    <ItemDescription>
-                      {Ong.description.substr(0, 55)}
-                    </ItemDescription>
-                  </CardItem>
-                ))}
+                {[...Ongs, ...Ongs].sort().map((Ong: any, index: any) => {
+                  return (
+                    <CardItem
+                      onPress={() => navigateDetails(Ong.id, index)}
+                      key={(Ong.id, index)}>
+                      <ImageUI
+                        source={{
+                          uri: pictures[index],
+                        }}
+                      />
+                      <ItemTitle>{Ong.name}</ItemTitle>
+                      <ItemDescription>
+                        {Ong.description.substr(0, 55)}
+                      </ItemDescription>
+                    </CardItem>
+                  );
+                })}
               </ListCardItem>
             </Container>
           </ScrollView>
